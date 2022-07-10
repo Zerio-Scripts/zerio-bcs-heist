ac57727a703b2c18473e3025d14a474e11720750
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-07-10 19:16:06 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+author "Zerio#0880"
+description "Free and open sourced bobcat security heist"
+repostitory "http://github.com/"
+
+shared_scripts {"config.lua", "lang.lua", "langs/en.lua"}
+
+server_scripts {'@oxmysql/lib/MySQL.lua', "server/functions.lua", "server/main.lua"}
+
+client_scripts {"client/functions.lua", "client/main.lua"}
+
+dependencies {"zerio-proximityprompt", "cfx-gabz-bobcat", "memorygame", "datacrack"}
+
+escrow_ignore {"*.lua", "**/*.lua"}
